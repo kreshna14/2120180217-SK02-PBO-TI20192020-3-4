5ecd17a42bf3afacc507cbf4cfb72a9ae870a178
kreshna14 <55661751+kreshna14@users.noreply.github.com>
2019-10-03 15:05:54 +0700

    upload tugas studi kasus 02

---
+++ 2120180217-SK02-PBO-TI20192020-3-4/src/pkg2120180217/sk02/pbo/ti20192020/pkg3/pkg4/SK02PBOTI2019202034.java
@@ -0,0 +1,20 @@
+
+package pkg2120180217.sk02.pbo.ti20192020.pkg3.pkg4;
+
+public class SK02PBOTI2019202034 {
+
+    public static void main(String[] args) {
+        smartphone smartphone_ku = new smartphone();
+            smartphone_ku.ukuran_layar = 5;
+            smartphone_ku.mg_camera = 8;
+            
+            System.out.println(smartphone_ku.jenis);
+            System.out.println(smartphone_ku.ukuran_layar);
+            System.out.println(smartphone_ku.mg_camera);
+            
+            smartphone_ku.nyala();
+            smartphone_ku.mati();
+            
+    }
+    
+}
---
+++ 2120180217-SK02-PBO-TI20192020-3-4/src/pkg2120180217/sk02/pbo/ti20192020/pkg3/pkg4/smartphone.java
@@ -0,0 +1,19 @@
+
+package pkg2120180217.sk02.pbo.ti20192020.pkg3.pkg4;
+
+public class smartphone {
+    
+//property
+    String jenis = "iphone"; 
+    int ukuran_layar;
+    int mg_camera;
+    
+    //method
+    void nyala(){
+        System.out.println("nyala");
+        
+    }
+    void mati(){
+        System.out.println("mati");
+    }
+}
